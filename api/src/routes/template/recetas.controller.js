008b0c781fc1aa2664e9062edb7f9803e1df7e34
Pablo Barbero <pablobarbero220@gmail.com>
2022-11-07 11:35:58 -0300

    routes files

---
+++ api/src/routes/template/recetas.controller.js
@@ -0,0 +1,41 @@
+import { Team } from "./recetas.js";
+
+export const getAllTeams = async (req, res) => {
+  const teams = await Team.find();
+
+  res.status(200).json(teams);
+};
+
+export const getTeam = async (req, res) => {
+  const { name } = req.params;
+  const team = await Team.find({ name });
+
+  res.status(200).json(team);
+};
+
+export const createTeam = async (req, res) => {
+  try {
+    const teams = req.body;
+
+    if (teams.length) {
+      const newTeams = await Team.create(teams);
+      return res.status(201).json(newTeams);
+    }
+
+    const { teamName, group } = req.body;
+    if (!teamName) {
+      return res.status(403).send("Can not create an empty teamName");
+    }
+
+    const team = new Team({
+      name: teamName,
+      group,
+      points: 0,
+    });
+    const newTeam = await team.save();
+
+    res.status(201).json(newTeam);
+  } catch (error) {
+    res.status(409).send(error.message);
+  }
+};
