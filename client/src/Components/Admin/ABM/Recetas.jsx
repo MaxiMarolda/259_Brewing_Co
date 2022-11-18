53d84b0e957746b9a9c260b31137cdd6043ea15d
Pablo Barbero <pablobarbero220@gmail.com>
2022-11-18 15:00:51 -0300

    refactoring recetas

--- client/src/Components/Admin/ABM/Recetas.jsx
+++ client/src/Components/Admin/ABM/Recetas.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useQuery } from "react-query";
+import styles from "./Recetas.module.css";
 
 const Recetas = () => {
   const getGroups = async (route) => {
@@ -10,13 +11,50 @@ const Recetas = () => {
 
   const recetas = useQuery("recetas", () => getGroups("receta"));
 
+  const columns = ["Producto", "Ingrediente", "Tipo", "grs", "grs/lt"];
+
   return (
     <div>
-      <p> ESTAS EN Recetas</p>
+      <h1> Recetas </h1>
 
       {recetas.status === "loading" && <div> Cargando recetas </div>}
       {recetas.status === "error" && <div> Error al cargar las recetas </div>}
-      {recetas.status === "success" &&
+      {recetas.status === "success" && (
+        <table className={styles.recetas}>
+          {recetas.data?.map((receta) => (
+            <tbody>
+              <tr>
+                {columns?.map((column, index) => (
+                  <th key={index}>{column}</th>
+                ))}
+              </tr>
+              {receta?.ingredients.map((ingredient, index) => (
+                <tr key={index}>
+                  {index === 0 ? (
+                    <td>Nombre: {receta.name}</td>
+                  ) : index === 1 ? (
+                    <td>Tipo: {receta.type}</td>
+                  ) : (
+                    <td></td>
+                  )}
+                  <td>{ingredient.name}</td>
+                  <td>{ingredient.type}</td>
+                  <td>{ingredient.grs}</td>
+                  <td>{ingredient.grs / 20}</td>
+                </tr>
+              ))}
+            </tbody>
+          ))}
+        </table>
+      )}
+    </div>
+  );
+};
+
+export default Recetas;
+
+/*
+{recetas.status === "success" &&
         recetas.data?.map((receta) => (
           <div key={receta._id}>
             <h2>
@@ -36,49 +74,5 @@ const Recetas = () => {
               })}
             </ul>
           </div>
-        ))}
-    </div>
-  );
-};
-
-export default Recetas;
-
-/*
-const Recetas = () => {
-  const getData = async (route) => {
-    console.log("@@", route);
-    const response = await fetch(`http://localhost:3001/receta`);
-    return response.json();
-  };
-
-  const { data, status } = useQuery("recetas", getData());
-
-  //const { data, status } = useQuery("recetas", getData);
-
-  //const materiasPrimas = useQuery("materiasPrimas", getData("materiaprima"));
-
-  return (
-    <div>
-      <p> ESTAS EN Recetas</p>
-      {status === "loading" && <div> Cargando recetas </div>}
-      {status === "error" && <div> Error al cargar las recetas </div>}
-      {status === "success" &&
-        data?.map((receta) => (
-          <p key={receta._id}>
-            {receta.name} {receta.type} {receta.ingredients[0].grs}
-          </p>
-        ))}
-      {materiasPrimas.status === "loading" && <div> Cargando materia prima </div>}
-      {materiasPrimas.status === "error" && <div> Error al cargar las materia prima </div>}
-      {materiasPrimas.status === "success" &&
-        materiasPrimas.data?.map((materiaPrima) => (
-          <p key={materiaPrima._id}>
-            {materiaPrima.name} {materiaPrima.type}
-          </p>
-        ))}
-        </div>
-        );
-      };
-      
-      export default Recetas;   
+        ))}  
 */
