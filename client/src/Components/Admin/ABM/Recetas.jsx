01c54f0b20acd37641a48475f252da1c418cd814
Pablo Barbero <pablobarbero220@gmail.com>
2022-11-19 21:43:37 -0300

    feat client recetas styles

--- client/src/Components/Admin/ABM/Recetas.jsx
+++ client/src/Components/Admin/ABM/Recetas.jsx
@@ -1,78 +1,98 @@
 import React from "react";
 import { useQuery } from "react-query";
-import styles from "./Recetas.module.css";
+import { DownOutlined } from "@ant-design/icons";
+import { Badge, Dropdown, Space, Table } from "antd";
 
 const Recetas = () => {
-  const getGroups = async (route) => {
-    console.log(route);
+  const getRecetas = async (route) => {
     const response = await fetch("http://localhost:3001/" + route);
     return response.json();
   };
 
-  const recetas = useQuery("recetas", () => getGroups("receta"));
+  const recetas = useQuery("recetas", () => getRecetas("receta"));
 
-  const columns = ["Producto", "Ingrediente", "Tipo", "grs", "grs/lt"];
+  const expandedRowRender = () => {
+    const columns = [
+      {
+        title: "Ingrediente",
+        dataIndex: "name",
+        key: "name",
+      },
+      {
+        title: "Tipo",
+        dataIndex: "type",
+        key: "type",
+      },
+      {
+        title: "grs",
+        dataIndex: "grs",
+        key: "grs",
+      },
+      {
+        title: "grs/lt",
+        dataIndex: "grs/lt",
+        key: "grs/lt",
+      },
+    ];
+
+    let data = [];
+
+    recetas.data?.forEach((receta) => {
+      data = receta.ingredients.map((ingredient, index) => ({
+        key: index.toString(),
+        name: ingredient.name,
+        type: ingredient.type,
+        grs: ingredient.grs,
+        "grs/lt": ingredient.grs / 20,
+      }));
+
+      /* receta.ingredients.forEach((ingredient, index) => {
+        data[index].push({
+          name: ingredient.name,
+          type: ingredient.type,
+          grs: ingredient.grs,
+          "grs/lt": ingredient.grs / 20,
+        });
+      }); */
+    });
+    return <Table columns={columns} dataSource={data} pagination={false} />;
+  };
+
+  const mainColumns = [
+    {
+      title: "Nombre",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Tipo",
+      dataIndex: "type",
+      key: "type",
+    },
+  ];
+
+  const mainData = [];
+
+  recetas.data?.forEach((receta, index) => {
+    mainData.push({
+      key: index.toString(),
+      name: receta.name,
+      type: receta.type,
+    });
+  });
 
   return (
-    <div>
-      <h1> Recetas </h1>
-
-      {recetas.status === "loading" && <div> Cargando recetas </div>}
-      {recetas.status === "error" && <div> Error al cargar las recetas </div>}
-      {recetas.status === "success" && (
-        <table className={styles.recetas}>
-          {recetas.data?.map((receta) => (
-            <tbody>
-              <tr>
-                {columns?.map((column, index) => (
-                  <th key={index}>{column}</th>
-                ))}
-              </tr>
-              {receta?.ingredients.map((ingredient, index) => (
-                <tr key={index}>
-                  {index === 0 ? (
-                    <td>Nombre: {receta.name}</td>
-                  ) : index === 1 ? (
-                    <td>Tipo: {receta.type}</td>
-                  ) : (
-                    <td></td>
-                  )}
-                  <td>{ingredient.name}</td>
-                  <td>{ingredient.type}</td>
-                  <td>{ingredient.grs}</td>
-                  <td>{ingredient.grs / 20}</td>
-                </tr>
-              ))}
-            </tbody>
-          ))}
-        </table>
-      )}
-    </div>
+    <>
+      <p> ESTAS EN Productos</p>
+      <Table
+        columns={mainColumns}
+        expandable={{
+          expandedRowRender,
+        }}
+        dataSource={mainData}
+      />
+    </>
   );
 };
 
 export default Recetas;
-
-/*
-{recetas.status === "success" &&
-        recetas.data?.map((receta) => (
-          <div key={receta._id}>
-            <h2>
-              {receta.name} {receta.type}
-            </h2>
-            <ul>
-              <li>Nombre - Tipo - grs - grs/lt</li>
-              {receta?.ingredients.map((ingredient) => {
-                return (
-                  <li>
-                    {ingredient.name} {" - "}
-                    {ingredient.type} {" - "}
-                    {ingredient.grs} {" - "}
-                    {ingredient.grs / 20}
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        ))}  
-*/
