87974da3f99f8ac4466177e0ec625fa9ff322ecc
Maximiliano Marolda <mmarolda12@gmail.com>
2022-11-29 20:09:24 -0300

    FEAT add use of CustomTable for Barriles

--- client/src/Components/Admin/ABM/Barriles.jsx
+++ client/src/Components/Admin/ABM/Barriles.jsx
@@ -1,77 +1,9 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
-import { Button, Form, Input, Popconfirm, Table } from "antd";
+import React, { useEffect, useState } from "react";
 import { useQuery } from "react-query";
-import Notification from "./Notification";
+import CustomTable from "../../assets/CustomTable";
 
-const EditableContext = React.createContext(null);
-const EditableRow = ({ index, ...props }) => {
-  const [form] = Form.useForm();
-  return (
-    <Form form={form} component={false}>
-      <EditableContext.Provider value={form}>
-        <tr {...props} />
-      </EditableContext.Provider>
-    </Form>
-  );
-};
-const EditableCell = ({ title, editable, children, dataIndex, record, handleSave, ...restProps }) => {
-  const [editing, setEditing] = useState(false);
-  const inputRef = useRef(null);
-  const form = useContext(EditableContext);
-  useEffect(() => {
-    if (editing) {
-      inputRef.current.focus();
-    }
-  }, [editing]);
-  const toggleEdit = () => {
-    setEditing(!editing);
-    form.setFieldsValue({
-      [dataIndex]: record[dataIndex],
-    });
-  };
-  const save = async () => {
-    try {
-      const values = await form.validateFields();
-      toggleEdit();
-      handleSave({
-        ...record,
-        ...values,
-      });
-    } catch (errInfo) {
-      console.log("Save failed:", errInfo);
-    }
-  };
-  let childNode = children;
-  if (editable) {
-    childNode = editing ? (
-      <Form.Item
-        style={{
-          margin: 0,
-        }}
-        name={dataIndex}
-        rules={[
-          {
-            required: true,
-            message: `${title} is required.`,
-          },
-        ]}
-      >
-        <Input ref={inputRef} onPressEnter={save} onBlur={save} />
-      </Form.Item>
-    ) : (
-      <div
-        className="editable-cell-value-wrap"
-        style={{
-          paddingRight: 24,
-        }}
-        onClick={toggleEdit}
-      >
-        {children}
-      </div>
-    );
-  }
-  return <td {...restProps}>{childNode}</td>;
-};
+
+
 
 const Barriles = () => {
   const getBarriles = async (route) => {
@@ -110,26 +42,23 @@ const Barriles = () => {
     };
   }, [barriles.status]);
 
-  const [count, setCount] = useState(2);
-  const handleDelete = (key) => {
-    const newData = dataSource.filter((item) => item.key !== key);
-    setDataSource(newData);
-  };
-  const defaultColumns = [
+  const dataColumns = [
     {
       title: "id",
       dataIndex: "_id",
-      width: "30%",
+      width: "20%",
       editable: false,
     },
     {
       title: "tipo",
       dataIndex: "type",
+      width: "10%",
       editable: true,
     },
     {
       title: "Cliente",
       dataIndex: "clientId",
+      width: "15%",
       editable: true,
     },
     {
@@ -147,99 +76,15 @@ const Barriles = () => {
       dataIndex: "isActive",
       editable: false,
     },
-    {
-      title: "operation",
-      dataIndex: "operation",
-      render: (_, record) =>
-        dataSource.length >= 1 ? (
-          <div>
-            <Popconfirm title="Seguro de querer borrar?" onConfirm={() => handleDelete(record.key)}>
-              <a>Delete</a>
-            </Popconfirm>{" "}
-          </div>
-        ) : null,
-    },
-    {
-      title: "operation2",
-      dataIndex: "operation2",
-      render: (_, record) =>
-        dataSource.length >= 1 ? (
-          <Notification
-            title="Barriles"
-            successMessage="Nuevo Barril guardado con éxito"
-            errorMessage="Error al guardar información"
-            id={record._id}
-            requestOptions={{
-              method: "PUT",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify(record),
-            }}
-          />
-        ) : null,
-    },
   ];
 
-  const handleAdd = () => {
-    const newData = {
-      key: count,
-      type: "Tipo",
-    };
-    setDataSource([...dataSource, newData]);
-    setCount(count + 1);
-  };
-
-  const handleSave = (row) => {
-    const newData = [...dataSource];
-    const index = newData.findIndex((item) => row.key === item.key);
-    const item = newData[index];
-    newData.splice(index, 1, {
-      ...item,
-      ...row,
-    });
-    setDataSource(newData);
-  };
-
-  const components = {
-    body: {
-      row: EditableRow,
-      cell: EditableCell,
-    },
-  };
-
-  const columns = defaultColumns.map((col) => {
-    if (!col.editable) {
-      return col;
-    }
-    return {
-      ...col,
-      onCell: (record) => ({
-        record,
-        editable: col.editable,
-        dataIndex: col.dataIndex,
-        title: col.title,
-        handleSave,
-      }),
-    };
-  });
 
   return (
     <div>
-      <Button
-        onClick={handleAdd}
-        type="primary"
-        style={{
-          marginBottom: 16,
-        }}
-      >
-        Add a row
-      </Button>
-      <Table
-        components={components}
-        rowClassName={() => "editable-row"}
-        bordered
-        dataSource={dataSource}
-        columns={columns}
-      />
+      <CustomTable 
+        dataColumns={dataColumns}
+        originData={handleData}
+        />
     </div>
   );
 };
